06a8a2cb250a07f5f664f508abd97d847f426e9b
Desmond Brown <browndesmond30@yahoo.com>
2020-10-07 12:14:55 +0000

    Kali: Added Static Files

---
+++ server/public/views/precache-manifest.944c21f01e9648d4f503db72ce4c4ec5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df0f4ad3fd52e7702b459a66337d39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f03bd1022890a0a412ea",
+    "url": "/static/css/main.f0271476.chunk.css"
+  },
+  {
+    "revision": "ec1e08093c4c10a23c9c",
+    "url": "/static/js/2.e8e12c8d.chunk.js"
+  },
+  {
+    "revision": "06170399e4f9afdaba2ec8c3ab5cdfb5",
+    "url": "/static/js/2.e8e12c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03bd1022890a0a412ea",
+    "url": "/static/js/main.136bbd34.chunk.js"
+  },
+  {
+    "revision": "45017a676713d713cffc",
+    "url": "/static/js/runtime-main.b34f3fc3.js"
+  },
+  {
+    "revision": "f5a0844bf578d57cef456f52d9a27996",
+    "url": "/static/media/loader-dual.f5a0844b.svg"
+  },
+  {
+    "revision": "5bde62cc94a57c35395d0958b1443c24",
+    "url": "/static/media/photo-33.5bde62cc.jpg"
+  },
+  {
+    "revision": "78dde446eb03dff8275f225962c9f9cf",
+    "url": "/static/media/photo-34.78dde446.jpg"
+  },
+  {
+    "revision": "a6d35c5afcb956544db1ae9846b7addc",
+    "url": "/static/media/photo-52.a6d35c5a.jpg"
+  },
+  {
+    "revision": "7f4c05b9998c98f2359fe45fbdf233c2",
+    "url": "/static/media/photo-53.7f4c05b9.jpg"
+  },
+  {
+    "revision": "094a516c098b4e2ea8a3421a2437df97",
+    "url": "/static/media/photo-54.094a516c.jpg"
+  },
+  {
+    "revision": "073b0f5e77f25f72cbadf4ceff1af54c",
+    "url": "/static/media/photo-55.073b0f5e.jpg"
+  },
+  {
+    "revision": "531a920c930701f48337ff8b968401ee",
+    "url": "/static/media/photo-56.531a920c.jpg"
+  },
+  {
+    "revision": "0e5eb014e0f89346aecee9edfa692952",
+    "url": "/static/media/photo-93.0e5eb014.jpg"
+  },
+  {
+    "revision": "111a66a75638344c8f03b0d48519157c",
+    "url": "/static/media/profile_woman.111a66a7.svg"
+  }
+]);
